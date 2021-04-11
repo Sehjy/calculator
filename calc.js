740d2c8883654c47fc53d1e15641ecfa9d394c43
Sehjveer Singh <sehjveer@gmail.com>
2021-04-10 23:26:30 -0700

    basic button and screen layout

---
+++ calc.js
@@ -0,0 +1,16 @@
+//FUNCTIONS ----------------------------------------------------
+function add(x, y) {
+	return x + y;
+}
+
+function subtract(x, y) {
+	return x - y;
+}
+
+function multiply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	return x / y;
+}
